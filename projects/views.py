2f2dd69252244e87dbd134740a1ddb9993b7f98c
Alan Xavier <alan_xvr@hotmail.com>
2022-09-09 05:27:59 +0200

    Create projects views and url paths

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from projects.models import Project
 
-# Create your views here.
+
+def project_index(request):
+    """View function for index page showing snippets of each project
+
+    Args:
+        request: HttpRequest
+    """
+    # Query collects all abjects from Projects table in database
+    projects = Project.objects.all()
+    # Context dictionary to send to template
+    context = {
+        'project': projects
+    }
+    return render(request, 'project_index.html', context)
+
+
+def project_detail(request, pk):
+    """View function for index page showing snippets of each project
+
+    Args:
+        request: HttpRequest
+        pk: Primary Key
+    """
+    # Query retrieves the project with primary key, pk
+    project = Project.objects.get(pk=pk)
+    context = {
+        'project': project
+    }
+    return render(request, 'project_detail.html', context)
